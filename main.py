b276b4eee88a3d2dcf2528abc72c2d282f4ba796
shawn <shawnhutchins1@gmail.com>
2025-10-05 20:32:51 -0400

    Read in csv data with pandas

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import turtle
+import pandas
 
 screen = turtle.Screen()
 screen.title("U.S.States Game")
 image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
+
+data = pandas.read_csv("50_states.csv")
+states = data.state
 
 answer_state = (screen.textinput(
     title="Guess the state",
