dd3007e596894f3f158f18f0c410fe7cb410206a
shawn <shawnhutchins1@gmail.com>
2025-10-05 20:57:12 -0400

    Checking and handling repeat correct guesses

--- main.py
+++ main.py
@@ -9,16 +9,21 @@ turtle.shape(image)
 
 data = pandas.read_csv("50_states.csv")
 states = data.state
-correct_guesses = 0
+correct_guess_count = 0
+correct_guesses = []
 
 game_is_on = True
 while game_is_on:
 
     answer_state = (screen.textinput(
-        title=f"{correct_guesses}/50 States Correct",
+        title=f"{correct_guess_count}/50 States Correct",
         prompt="What's another state's name?")
         .title())
     print(answer_state)
 
     if answer_state in states.values:
-        correct_guesses += 1
+        if answer_state in correct_guesses:
+            print(f"You already correctly guessed {answer_state}")
+        else:
+            correct_guesses.append(answer_state)
+            correct_guess_count += 1
